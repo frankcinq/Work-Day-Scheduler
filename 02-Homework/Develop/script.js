7fee9191a9ab262135f3f578d6fe1c81d8cc5aa3
Francisco Almaraz <frankcinq@yahoo.com>
2021-08-26 01:46:40 -0500

    fixed columm size and added javascript

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -1,3 +1,15 @@
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 
+
+var objDate = new Date();
+    var hours = objDate.getHours();
+    if(hours >= 9 && hours <= 17) {
+        $("textarea").addClass("present");
+    }
+    else if(hours < 9) {
+        $("textarea").addClass("past");
+    }
+    else {
+        $("textarea").addClass("future");
+    }

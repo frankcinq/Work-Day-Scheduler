680525a816ca02c0a98ee33f67aabd80e4acd0b4
Francisco Almaraz <frankcinq@yahoo.com>
2021-08-28 22:05:24 -0500

    added for loop and hours working

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -1,15 +1,47 @@
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 
+var timeIds = [];
+
+$(".time-block").each(function () {
+  timeIds.push(this.id);
+  console.log(timeIds);
+});
 
 var objDate = new Date();
-    var hours = objDate.getHours();
-    if(hours >= 9 && hours <= 17) {
-        $("textarea").addClass("present");
-    }
-    else if(hours < 9) {
-        $("textarea").addClass("past");
-    }
-    else {
-        $("textarea").addClass("future");
-    }
+var hours = objDate.getHours();
+console.log(hours);
+for (var i = 0; i < timeIds.length; i++) {
+  if (parseInt(timeIds[i]) === hours) {
+    $("#" + timeIds[i]).addClass("present");
+    console.log(timeIds[i]);
+    console.log(hours);
+  } else if (parseInt(timeIds[i]) < hours) {
+    $("#" + timeIds[i]).addClass("past");
+  } else {
+    $("#" + timeIds[i]).addClass("future");
+  }
+}
+
+/*renderLastRegistered();
+
+function renderLastRegistered() {
+    var textArea = localStorage.getItem("textarea");
+}
+  */
+for (var i = 0; i < textarea.length; i++)
+  $(".saveBtn").on("click", function () {
+    var textArea = document.querySelectorAll("textarea").value;
+    console.log(textArea);
+  });
+
+function mySave() {
+  var myContent = document.querySelector("textarea").value;
+  localStorage.setItem("textarea", textarea);
+}
+function myLoad() {
+  var myContent = localStorage.getItem("textarea");
+  document.querySelectorAll("textarea").value = myContent;
+}
+
+//35 = value,
